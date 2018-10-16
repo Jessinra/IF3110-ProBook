51d375b3cc3cf1b8efefd174edc0551a03ebfbba
JessinRa <jessin.bdg@gmail.com>
2018-10-16 21:15:47 +0700

    Created Header display, add View folder

---
+++ View/template_with_header.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>WBDee Bookstore</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="../View/CSS/header.css">
+</head>
+<body>
+
+<?php
+include("../View/header.html");
+?>
+
+</body>
+</html>
