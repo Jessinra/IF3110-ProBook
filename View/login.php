178d9ef107ce9da1118bd7fc080664842d1fc639
JessinRa <jessin.bdg@gmail.com>
2018-10-17 15:13:19 +0700

    search page, custom font added, html to 40% width, fix font & font-size, resolve login-page's warning

--- View/login.php
+++ View/login.php
@@ -2,32 +2,34 @@
 <html>
 
 <head>
-  <title>Pro-Book</title>
-  <link rel="stylesheet" type="text/css" href="css/login.css">
+    <title>Pro-Book</title>
+    <link rel="stylesheet" type="text/css" href="../View/CSS/login.css">
 </head>
 
 <body>
-  <div class="form">
+<div class="form">
     <div class="box">
-      <div class="judul">
-        <div id="tengah"><h1>LOGIN</h1></div>
-      </div>
-      <form action="#" method="post" onsubmit="return validateLogin()">
-        <table id="login">
-          <tr>
-            <th class="masuk"><label>Username</label></th>
-            <th><input id="username" type="text" name="username" placeholder="" /></th>
-          </tr>
-          <tr>
-            <th class="masuk"><label>Password</label></th>
-            <th><input id="password" type="password" name="password" placeholder="" /></th>
-          </tr>
-        </table>
-        <label id="pindah"><a href="#">Don't have an account?</a></label>
-        <br>
-        <button type="submit" id="submit" name="login" />LOGIN</button>
-      </form>
+        <div class="judul">
+            <div id="tengah"><h1>LOGIN</h1></div>
+        </div>
+        <form action="#" method="post" onsubmit="return validateLogin()">
+            <table id="login">
+                <tr>
+                    <th class="masuk"><label>Username</label></th>
+                    <th><input id="username" type="text" name="username" placeholder=""/></th>
+                </tr>
+                <tr>
+                    <th class="masuk"><label>Password</label></th>
+                    <th><input id="password" type="password" name="password" placeholder=""/></th>
+                </tr>
+            </table>
+            <label id="pindah"><a href="#">Don't have an account?</a></label>
+            <br>
+            <button type="submit" id="submit" name="login">
+                LOGIN
+            </button>
+        </form>
     </div>
-  </div>
+</div>
 </body>
 </html>
--- View/search.php
+++ View/search.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="../View/CSS/header.css">
     <link rel="stylesheet" href="../View/CSS/style.css">
+    <link rel="stylesheet" href="../View/CSS/search.css">
 </head>
 <body>
 
@@ -16,15 +17,15 @@
     ?>
 </div>
 
-<div class="">
-
+<div class="page-title">
+    <h1 id="page-title-search">Search Book</h1>
 </div>
-<div>
-
+<div class="container search-bar">
+    <div class="search-bar field">
+        <input type="text" id="form-search-query" placeholder="Input search terms..."/>
+    </div>
+    <button class="search-bar button" type="submit">Search</button>
 </div>
-
-
-
 
 
 </body>
