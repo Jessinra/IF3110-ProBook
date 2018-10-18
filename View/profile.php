7757fd9d0a9d6093d3576a01a8fb2d4800f02297
Tony <buddy90_lost@yahoo.co.id>
2018-10-18 17:25:37 +0700

    add profile detail

--- View/profile.php
+++ View/profile.php
@@ -36,26 +36,52 @@
                 <h1>My Profile</h1>
             </div>
             <table>
-                <tr class="profile username">
+                <tr class="field" id="profile-username">
                     <td class="icon-container">
                         <img class="icon" src="../img/user.png" />
                     </td>
-                    <td>
-                        <img class="icon" src="../img/user.png" />
+                    <td class="label">
+                        Name
                     </td>
-                    <td>
-                        
+                    <td class="content" id="username">
+                        <!-- UserName -->
+                        @legalloli
                     </td>
                 </tr>
-                <tr>
-                    <td>
-
+                <tr class="field" id="profile-email">
+                    <td class="icon-container">
+                        <img class="icon" src="../img/email.png" />
                     </td>
-                    <td>
-                        
+                    <td class="label">
+                        Email
                     </td>
-                    <td>
-
+                    <td class="content" id="email">
+                        <!-- Email -->
+                        loli@gmail.com
+                    </td>
+                </tr>
+                <tr class="field" id="profile-address">
+                    <td class="icon-container">
+                        <img class="icon" src="../img/address.png" />
+                    </td>
+                    <td class="label">
+                        Address
+                    </td>
+                    <td class="content" id="address">
+                        <!-- Address -->
+                        Jl. Ganesha No. 10
+                    </td>
+                </tr>
+                <tr class="field" id="profile-phone-number">
+                    <td class="icon-container">
+                        <img class="icon" src="../img/phone.png" />
+                    </td>
+                    <td class="label">
+                        Phone Number
+                    </td>
+                    <td class="content" id="phone-number">
+                        <!-- Phone Number -->
+                        08123456789
                     </td>
                 </tr>
             </table>
