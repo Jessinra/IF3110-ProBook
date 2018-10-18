5a940dcd167d8471dec20e94512d6153c055f9d9
JessinRa <jessin.bdg@gmail.com>
2018-10-18 14:28:56 +0700

    mock data, fixed top header

--- View/search-result.php
+++ View/search-result.php
@@ -17,104 +17,106 @@
     ?>
 </div>
 
+
+<!--Setup dictionary as query result (dummy) -->
+
+<?php
+$query_result = array();
+
+$query_entry = array(
+    'thumbnail' => "../View/Src/search_result/thumbnail_01.png",
+    'title' => "Platelets are cute !",
+    'author' => "Kaito Yamamura",
+    'ratings' => 4.8,
+    'max_rating' => 5.0,
+    'votes' => 10000,
+    'description' => 'Lorem ipsum dolor sit amet, populo scripserit eos id, vim probo solum forensibus ei. Autem prompta
+                consequuntur an ius, sint adhuc ex usu. No his tota dicant, vis aeterno suscipiantur no, an assentior
+                intellegebat cum. Falli homero id mea.',
+);
+$query_result[0] = $query_entry;
+$query_result[2] = $query_entry;
+$query_result[4] = $query_entry;
+$query_result[6] = $query_entry;
+$query_result[8] = $query_entry;
+
+$query_entry = array(
+    'thumbnail' => "../View/Src/search_result/thumbnail_03.png",
+    'title' => "Megumi so cute !",
+    'author' => "Yamada Shimamura",
+    'ratings' => 5.8,
+    'max_rating' => 5.0,
+    'votes' => 1000000,
+    'description' => 'Lorem ipsum dolor sit amet, populo scripserit eos id, vim probo solum forensibus ei. Autem prompta
+                consequuntur an ius, sint adhuc ex usu. No his tota dicant, vis aeterno suscipiantur no, an assentior
+                intellegebat cum. Falli homero id mea.',
+);
+$query_result[1] = $query_entry;
+$query_result[3] = $query_entry;
+$query_result[5] = $query_entry;
+$query_result[7] = $query_entry;
+
+ksort($query_result);
+
+?>
+
 <div class="page-title">
     <h1 id="page-title-search">Search Result</h1>
-    <span id="search-result-found">Found <u>3</u> result(s)</span>
+    <span id="search-result-found">Found <u><?php echo sizeof($query_result)?></u> result(s)</span>
 </div>
 
-<div class="search-result container">
-    <div class="flex-container">
-        <div class="search-result thumbnail">
-            <img src="../View/Src/search_result/thumbnail_01.png" alt="search thumbnail">
+<?php foreach ($query_result as $query_entry) { ?>
+    <div class="search-result container">
+        <div class="flex-container">
+            <div class="search-result thumbnail">
+                <img src= "<?php echo $query_entry['thumbnail'] ?>" alt="search thumbnail">
+            </div>
+            <div class="search-result description">
+                <div class="title">
+                    <?php echo $query_entry['title'] ?>
+                </div>
+                <div class="author">
+                    <span class="author-name"><?php echo $query_entry['author'] ?></span> -
+                    <span class="ratings"><?php echo $query_entry['ratings'] ?>/<?php echo $query_entry['max_rating'] ?></span>
+                    <span class="votes">(<?php echo $query_entry['votes'] ?> votes)</span>
+
+                </div>
+                <div class="description">
+                    <?php echo $query_entry['description'] ?>
+                </div>
+
+            </div>
         </div>
-        <div class="search-result description">
-            <div class="title">
-                Platelets are cute !
-            </div>
-            <div class="author">
-                <span class="author-name">Kaito Yamamura</span> -
-                <span class="ratings">4.8/5.0</span>
-                <span class="votes">(10000 votes)</span>
 
-            </div>
-            <div class="description">
-                Lorem ipsum dolor sit amet, populo scripserit eos id, vim probo solum forensibus ei. Autem prompta
-                consequuntur an ius, sint adhuc ex usu. No his tota dicant, vis aeterno suscipiantur no, an assentior
-                intellegebat cum. Falli homero id mea.
-            </div>
-
-        </div>
-    </div>
-
-    <div>
-        <a href="#">
-            <button class="search-result button">Detail</button>
-        </a>
-    </div>
-</div>
-
-
-<div class="search-result container">
-    <div class="flex-container">
-        <div class="search-result thumbnail">
-            <img src="../View/Src/search_result/thumbnail_02.png" alt="search thumbnail">
-        </div>
-        <div class="search-result description">
-            <div class="title">
-                Platelets are cute !
-            </div>
-            <div class="author">
-                <span class="author-name">Kaito Yamamura</span> -
-                <span class="ratings">4.8/5.0</span>
-                <span class="votes">(10000 votes)</span>
-
-            </div>
-            <div class="description">
-                Lorem ipsum dolor sit amet, populo scripserit eos id, vim probo solum forensibus ei. Autem prompta
-                consequuntur an ius, sint adhuc ex usu. No his tota dicant, vis aeterno suscipiantur no, an assentior
-                intellegebat cum. Falli homero id mea.
-            </div>
-
+        <div>
+            <a href="#">
+                <button class="search-result button">Detail</button>
+            </a>
         </div>
     </div>
+<?php } ?>
 
-    <div>
-        <a href="#">
-            <button class="search-result button">Detail</button>
-        </a>
-    </div>
-</div>
 
-<div class="search-result container">
-    <div class="flex-container">
-        <div class="search-result thumbnail">
-            <img src="../View/Src/search_result/thumbnail_03.png" alt="search thumbnail">
-        </div>
-        <div class="search-result description">
-            <div class="title">
-                Platelets are cute !
-            </div>
-            <div class="author">
-                <span class="author-name">Kaito Yamamura</span> -
-                <span class="ratings">4.8/5.0</span>
-                <span class="votes">(10000 votes)</span>
-
-            </div>
-            <div class="description">
-                Lorem ipsum dolor sit amet, populo scripserit eos id, vim probo solum forensibus ei. Autem prompta
-                consequuntur an ius, sint adhuc ex usu. No his tota dicant, vis aeterno suscipiantur no, an assentior
-                intellegebat cum. Falli homero id mea.
-            </div>
-
-        </div>
-    </div>
-
-    <div>
-        <a href="#">
-            <button class="search-result button">Detail</button>
-        </a>
-    </div>
-</div>
 
 </body>
+
+<!--Make nav bar static at top-->
+<script>
+    window.onscroll = function() {stick_nav_to_top()};
+
+    let nav_bar = document.getElementsByClassName("nav-bar")[0];
+    let sticky_nav_bar = nav_bar.offsetTop;
+
+    function stick_nav_to_top() {
+        if (window.pageYOffset > sticky_nav_bar) {
+            nav_bar.classList.add("sticky-nav-bar");
+            nav_bar.classList.add("resp-element");
+        } else {
+            nav_bar.classList.remove("sticky-nav-bar");
+            nav_bar.classList.remove("resp-element");
+        }
+    }
+</script>
+
+
 </html>
