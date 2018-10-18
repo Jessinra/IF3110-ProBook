68b930412b5e9f2a342c24219ce34972ef4b0289
JessinRa <jessin.bdg@gmail.com>
2018-10-18 12:18:47 +0700

    search result page, optimize css, fix header - navbar styling

---
+++ View/search-result.php
@@ -0,0 +1,120 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>WBDee Bookstore</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="../View/CSS/header.css">
+    <link rel="stylesheet" href="../View/CSS/style.css">
+    <link rel="stylesheet" href="../View/CSS/search.css">
+</head>
+<body>
+
+<div class="header">
+    <?php
+    include("../View/header.html");
+    include("../View/nav-bar.html");
+    ?>
+</div>
+
+<div class="page-title">
+    <h1 id="page-title-search">Search Result</h1>
+    <span id="search-result-found">Found <u>3</u> result(s)</span>
+</div>
+
+<div class="search-result container">
+    <div class="flex-container">
+        <div class="search-result thumbnail">
+            <img src="../View/Src/search_result/thumbnail_01.png" alt="search thumbnail">
+        </div>
+        <div class="search-result description">
+            <div class="title">
+                Platelets are cute !
+            </div>
+            <div class="author">
+                <span class="author-name">Kaito Yamamura</span> -
+                <span class="ratings">4.8/5.0</span>
+                <span class="votes">(10000 votes)</span>
+
+            </div>
+            <div class="description">
+                Lorem ipsum dolor sit amet, populo scripserit eos id, vim probo solum forensibus ei. Autem prompta
+                consequuntur an ius, sint adhuc ex usu. No his tota dicant, vis aeterno suscipiantur no, an assentior
+                intellegebat cum. Falli homero id mea.
+            </div>
+
+        </div>
+    </div>
+
+    <div>
+        <a href="#">
+            <button class="search-result button">Detail</button>
+        </a>
+    </div>
+</div>
+
+
+<div class="search-result container">
+    <div class="flex-container">
+        <div class="search-result thumbnail">
+            <img src="../View/Src/search_result/thumbnail_02.png" alt="search thumbnail">
+        </div>
+        <div class="search-result description">
+            <div class="title">
+                Platelets are cute !
+            </div>
+            <div class="author">
+                <span class="author-name">Kaito Yamamura</span> -
+                <span class="ratings">4.8/5.0</span>
+                <span class="votes">(10000 votes)</span>
+
+            </div>
+            <div class="description">
+                Lorem ipsum dolor sit amet, populo scripserit eos id, vim probo solum forensibus ei. Autem prompta
+                consequuntur an ius, sint adhuc ex usu. No his tota dicant, vis aeterno suscipiantur no, an assentior
+                intellegebat cum. Falli homero id mea.
+            </div>
+
+        </div>
+    </div>
+
+    <div>
+        <a href="#">
+            <button class="search-result button">Detail</button>
+        </a>
+    </div>
+</div>
+
+<div class="search-result container">
+    <div class="flex-container">
+        <div class="search-result thumbnail">
+            <img src="../View/Src/search_result/thumbnail_03.png" alt="search thumbnail">
+        </div>
+        <div class="search-result description">
+            <div class="title">
+                Platelets are cute !
+            </div>
+            <div class="author">
+                <span class="author-name">Kaito Yamamura</span> -
+                <span class="ratings">4.8/5.0</span>
+                <span class="votes">(10000 votes)</span>
+
+            </div>
+            <div class="description">
+                Lorem ipsum dolor sit amet, populo scripserit eos id, vim probo solum forensibus ei. Autem prompta
+                consequuntur an ius, sint adhuc ex usu. No his tota dicant, vis aeterno suscipiantur no, an assentior
+                intellegebat cum. Falli homero id mea.
+            </div>
+
+        </div>
+    </div>
+
+    <div>
+        <a href="#">
+            <button class="search-result button">Detail</button>
+        </a>
+    </div>
+</div>
+
+</body>
+</html>
