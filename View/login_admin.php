e4464d364e220ba9db962220e89ee018cdb99d2e
JessinRa <jessin.bdg@gmail.com>
2018-10-23 01:34:34 +0700

    login & register with flex style and background enabled

---
+++ View/login_admin.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Pro-Book</title>
+    <link rel="stylesheet" type="text/css" href="../View/CSS/style.css">
+    <link rel="stylesheet" type="text/css" href="CSS/login_admin.css">
+    <script type="text/javascript" src="JS/validate_login.js"></script>
+</head>
+
+<body>
+<div class="form">
+    <div class="box">
+        <div class="title">
+            <div id="middle"><h1>LOGIN</h1></div>
+        </div>
+        <form action="#" method="post" onsubmit="return validateLogin()">
+            <div class="flex-container login-container">
+                <div class="flex-container">
+                    <div class="form-label">
+                        <label>Username</label>
+                    </div>
+                    <div class="form-entry">
+                        <input id="username" type="text" name="username" placeholder="" onclick="this.select()"/>
+                    </div>
+                </div>
+                <div class="flex-container">
+                    <div class="form-label">
+                        <label>Password</label>
+                    </div>
+                    <div class="form-entry">
+                        <input id="password" type="password" name="password" placeholder="" onclick="this.select()"/>
+                    </div>
+                </div>
+            </div>
+            <div class="create-account-container">
+                <label id="move">
+                    <a href="../View/register_admin.php">Don't have an account?</a>
+                </label>
+            </div>
+            <div>
+                <a href="../View/search.php">
+                    <button type="button" id="submit" name="login">
+                        <span>LOGIN</span>
+                    </button>
+                </a>
+            </div>
+        </form>
+    </div>
+</div>
+</body>
+</html>
