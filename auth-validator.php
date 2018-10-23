03fdcea2f58ee62a2f11dcfaef4c68aca9b8d9db
Tony <buddy90_lost@yahoo.co.id>
2018-10-23 20:53:52 +0700

    git add auth validator

---
+++ auth-validator.php
@@ -0,0 +1,27 @@
+<?php
+    require_once '../config.php';
+    
+    if(basename(__FILE__) == 'login.php' || basename(__FILE__) == 'register.php'){
+        if(isset($_COOKIE['ID'])){
+            $userid = $_COOKIE['ID'];
+            $query_string = "SELECT `id` FROM `active_users` WHERE `id`='$userid'";
+            $query_result = $mysqli->query($query_string);
+            if(!empty($query_result)){
+                header('Location: search.php');
+            }
+        }
+    }
+    else{
+        if(!isset($_COOKIE['ID'])){
+            header('Location: login.php');
+        }
+        else{
+            $userid = $_COOKIE['ID'];
+            $query_string = "SELECT `id` FROM `active_users` WHERE `id`='$userid'";"
+            $query_result = $mysqli->query($query_string);
+            if(empty($query_result)){
+                header('Location: login.php');
+            }
+        }
+    }
+?>
