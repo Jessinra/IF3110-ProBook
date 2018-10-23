dc65bc1b95cdd4f8c5018050f945d07fb99dab73
Tony <buddy90_lost@yahoo.co.id>
2018-10-23 22:28:44 +0700

    add database query and change variabel name

--- App/profile.php
+++ App/profile.php
@@ -2,7 +2,15 @@
     require_once '../auth-validator.php';
     require_once '../config.php';
 
+    $user_id = $_COOKIE["ID"];
+    $query_string_profile = "SELECT `username`, `name`, `email`, `address`, `phone_number`, `img` FROM `users` WHERE `id` = $user_id;";
+    $query_result_profile = $mysqli->query($query_string_profile)->fetch_assoc();
 
+    if($query_result_profile['img'] === NULL){
+        $query_result_profile['img'] = 'default.jpg';
+    }
+
+    $query_result_profile['img'] = "../View/Src/profile/sample/".$query_result_profile['img'];
 
     require_once '../View/profile.php';
 ?>
