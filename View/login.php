423f3cfd1af4efad7cd690a0c65d2066307a83ba
Harry Setiawan Hamjaya <hamjaya_harry@yahoo.co.id>
2018-10-23 12:33:38 +0700

    Fixing error view in js and combining login and register with other page view

--- View/login.php
+++ View/login.php
@@ -3,7 +3,8 @@
 
 <head>
     <title>Pro-Book</title>
-    <link rel="stylesheet" type="text/css" href="../View/CSS/login.css">
+    <link rel="stylesheet" type="text/css" href="../View/CSS/style.css">
+    <link rel="stylesheet" type="text/css" href="CSS/login_admin.css">
     <script type="text/javascript" src="JS/validate_login.js"></script>
 </head>
 
@@ -11,25 +12,39 @@
 <div class="form">
     <div class="box">
         <div class="title">
-            <div id="middle"><h1>L O G I N</h1></div>
+            <div id="middle"><h1>LOGIN</h1></div>
         </div>
         <form action="#" method="post" onsubmit="return validateLogin()">
-            <table>
-                <tr>
-                    <th class="table_header"><label>Username</label></th>
-                    <th><input id="username" type="text" name="username" placeholder="" onclick="this.select()"/></th>
-                </tr>
-                <tr>
-                    <th class="table_header"><label>Password</label></th>
-                    <th><input id="password" type="password" name="password" placeholder="" onclick="this.select()"/>
-                    </th>
-                </tr>
-            </table>
-            <label id="move"><a href="register.php">Don't have an account?</a></label>
-            <br>
-            <button type="submit" id="submit" name="login">
-                L O G I N
-            </button>
+            <div class="flex-container login-container">
+                <div class="flex-container">
+                    <div class="form-label">
+                        <label>Username</label>
+                    </div>
+                    <div class="form-entry">
+                        <input id="username" type="text" name="username" placeholder="" onclick="this.select()"/>
+                    </div>
+                </div>
+                <div class="flex-container">
+                    <div class="form-label">
+                        <label>Password</label>
+                    </div>
+                    <div class="form-entry">
+                        <input id="password" type="password" name="password" placeholder="" onclick="this.select()"/>
+                    </div>
+                </div>
+            </div>
+            <div class="create-account-container">
+                <label id="move">
+                    <a href="../View/register_admin.php">Don't have an account?</a>
+                </label>
+            </div>
+            <div>
+                <a href="../View/search.php">
+                    <button type="button" id="submit" name="login">
+                        <span>LOGIN</span>
+                    </button>
+                </a>
+            </div>
         </form>
     </div>
 </div>
