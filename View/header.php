f77b8f34d7e54fbd9737b8b6fff841c870e479c4
Tony <buddy90_lost@yahoo.co.id>
2018-10-23 22:26:48 +0700

    update name variable in header.php

--- View/header.php
+++ View/header.php
@@ -3,11 +3,10 @@
 require_once '../config.php';
 
 $user_id = $_COOKIE['ID'];
-$query_result = $mysqli->query("SELECT username FROM users WHERE id=$user_id")->fetch_assoc();
+$username_query = $mysqli->query("SELECT username FROM users WHERE id=$user_id")->fetch_assoc();
 
-// echo $query_result[0];
-
-$username = $query_result['username'];
+$username = $username_query['username'];
+// $username = "oio";
 
 ?>
 
