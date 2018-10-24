c5dbe91892b222768cba913b321a76bcb2415447
JessinRa <jessin.bdg@gmail.com>
2018-10-24 14:19:09 +0700

    remove php tag from view/login.php

--- View/login.php
+++ View/login.php
@@ -1,5 +1,5 @@
 <?php
-    require '../config.php';
+
 ?>
 
 <!DOCTYPE html>
@@ -11,15 +11,6 @@
     <link rel="stylesheet" type="text/css" href="../View/CSS/login.css">
     <script type="text/javascript" src="JS/validate_login.js"></script>
 </head>
-
-<?php 
-if ($_SERVER['REQUEST_METHOD'] == 'POST') 
-{
-  if (isset($_POST['login'])) {
-    require '../App/login.php';
-  }
-}
-?>
 
 <body>
 <div class="form">
