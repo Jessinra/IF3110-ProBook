f05bd31f347d17a9ff9e8e16ef1f75cb0800ab49
Tony <buddy90_lost@yahoo.co.id>
2018-10-25 23:39:15 +0700

    add recieve response of get

--- App/review.php
+++ App/review.php
@@ -1,6 +1,25 @@
 <?php
     require_once '../App/auth-validator.php';
 
-    
-    require_once '../View/review.php';
+    $user_id = $_COOKIE["ID"];
+
+    if(isset($_GET["order-number"])){
+        $id_trans = $_GET["order-number"];
+        $query_string_trans = "SELECT `book_id` FROM `transactions` WHERE `id`=$id_trans AND `user_id`=$user_id AND `is_review`=0;";
+        $query_result_trans = $mysqli->query($query_string_trans);
+        if($query_result_trans->num_rows==1){
+            $pic_dir = "../View/Src/search_result/";
+            $book_id = $query_result_trans->fetch_assoc()["book_id"];
+            $query_string_book = "SELECT `title`, `author`, `img` FROM `books` WHERE `id`=$book_id;";
+            $query_result_book = $mysqli->query($query_string_book)->fetch_assoc();
+            $book_details = array(
+                "title" => $query_result_book['title'],
+                "thumbnail" => $pic_dir.$query_result_book['img'],
+                "author" => $query_result_book['author']
+            );
+            require_once '../View/review.php';
+        }
+        // ELSE require error page; 
+    }
+    // ELSE require error page;
 ?>
