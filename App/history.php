279c2fc4402e0e2b1be13ebf9ee3c6bdaae6d9db
Harry Setiawan Hamjaya <hamjaya_harry@yahoo.co.id>
2018-10-26 18:45:40 +0700

    Fixed sort for retrieving data by newest

--- App/history.php
+++ App/history.php
@@ -22,7 +22,8 @@
     $query_result_history = array();
     $query_string_history = $mysqli->query("SELECT transactions.order_date as order_date,transactions.id as id , transactions.amount as amount , transactions.is_review as review , books.img as thumbnail , books.title as title
     FROM transactions JOIN books on transactions.book_id=books.id
-    WHERE transactions.user_id = $user_id");
+    WHERE transactions.user_id = $user_id
+    ORDER BY order_date ASC");
 
     while( $r = $query_string_history->fetch_array()){
         $temp = (int) substr($r['order_date'], 5, 2);
